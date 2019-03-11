a357549287717d425e05ad8e2eb2231f5570350e
Florian Garcia <florian.garcia@outlook.fr>
2019-03-11 08:25:46 +0100

    Init settings

--- webapp/lightdash/views.py
+++ webapp/lightdash/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
 from django.conf import settings
+from lightdash.models import *
+from django.db.models import Count
+
+
+if(Settings.objects.all().count() == 0):
+    print("Initialize Settings...")
+    currentSettings = Settings(id=1,abonnementHPHC = True,tarifHP = 0.1579,tarifHC = 0.1228,debutHC = datetime.time(22,0),finHC = datetime.time(6,0))
+    currentSettings.save();
+else:
+    print("Get Settings...")
+    currentSettings = get_object_or_404(Settings, id=1)
+    
+
 # Create your views here.
 
 def index(request):
