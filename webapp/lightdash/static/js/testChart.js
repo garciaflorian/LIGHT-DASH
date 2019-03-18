0c04778e733252af37c85b8cfbd69d76ab53e99c
Florian Garcia <florian.garcia@outlook.fr>
2019-03-18 17:48:47 +0100

    clean testChart

--- webapp/lightdash/static/js/testChart.js
+++ webapp/lightdash/static/js/testChart.js
@@ -1,114 +1,56 @@
-console.log(randomScalingFactor());
-
-		var config = {
-			type: 'line',
-			data: {
-				labels: DATE,
-				datasets: [{
-					label: 'Puissance apparente',
-					backgroundColor: window.chartColors.red,
-					borderColor: window.chartColors.red,
-					data: PAPP,
-					fill: false,
-                    label: 'VA',
-                    yAxisID: 'VA',
+var config = {
+    type: 'line',
+    data: {
+        labels: DATE,
+        datasets: [{
+            label: 'Puissance apparente',
+            backgroundColor: window.chartColors.red,
+            borderColor: window.chartColors.red,
+            data: PAPP,
+            fill: false,
+            label: 'VA',
+            yAxisID: 'VA',
 				}]
-			},
-			options: {
-				responsive: true,
-				title: {
-					display: true,
-					text: 'Linky'
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-				scales: {
-					xAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Heures'
-						}
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Linky'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Heures'
+                }
 					}],
-					yAxes: [{
-						display: true,
-                        id: 'VA',
-                        position: 'left',
-						scaleLabel: {
-							display: true,
-						}
+            yAxes: [{
+                display: true,
+                id: 'VA',
+                position: 'left',
+                scaleLabel: {
+                    display: true,
+                },
+                ticks: {
+                    beginAtZero: true
+                }
 					}]
-                      }
-				}
-			
-		};
+        }
+    }
 
-		window.onload = function() {
-			var ctx = document.getElementById('canvas').getContext('2d');
-			window.myLine = new Chart(ctx, config);
-		};
+};
 
-		document.getElementById('randomizeData').addEventListener('click', function() {
-			config.data.datasets.forEach(function(dataset) {
-				dataset.data = dataset.data.map(function() {
-					return randomScalingFactor();
-				});
-
-			});
-
-			window.myLine.update();
-		});
-
-		var colorNames = Object.keys(window.chartColors);
-		document.getElementById('addDataset').addEventListener('click', function() {
-			var colorName = colorNames[config.data.datasets.length % colorNames.length];
-			var newColor = window.chartColors[colorName];
-			var newDataset = {
-				label: 'Dataset ' + config.data.datasets.length,
-				backgroundColor: newColor,
-				borderColor: newColor,
-				data: [],
-				fill: false
-			};
-
-			for (var index = 0; index < config.data.labels.length; ++index) {
-				newDataset.data.push(randomScalingFactor());
-			}
-
-			config.data.datasets.push(newDataset);
-			window.myLine.update();
-		});
-
-		document.getElementById('addData').addEventListener('click', function() {
-			if (config.data.datasets.length > 0) {
-				var month = MONTHS[config.data.labels.length % MONTHS.length];
-				config.data.labels.push(month);
-
-				config.data.datasets.forEach(function(dataset) {
-					dataset.data.push(randomScalingFactor());
-				});
-
-				window.myLine.update();
-			}
-		});
-
-		document.getElementById('removeDataset').addEventListener('click', function() {
-			config.data.datasets.splice(0, 1);
-			window.myLine.update();
-		});
-
-		document.getElementById('removeData').addEventListener('click', function() {
-			config.data.labels.splice(-1, 1); // remove the label first
-
-			config.data.datasets.forEach(function(dataset) {
-				dataset.data.pop();
-			});
-
-			window.myLine.update();
-		});
+window.onload = function () {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.myLine = new Chart(ctx, config);
+};
