8c6eaea6fc0e6efe5ecc8847ef5431fc7281347b
Florian Garcia <florian.garcia@outlook.fr>
2019-03-31 17:09:31 +0200

    Large test Dataset + HPHC calculation for month only

--- webapp/lightdash/views.py
+++ webapp/lightdash/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from lightdash.models import *
 from django.forms import ModelForm
 from django.db.models import Count
+from datetime import datetime, timedelta
 import json
 
 if(Settings.objects.all().count() == 0):
@@ -15,12 +16,56 @@ if(Settings.objects.all().count() == 0):
 # Create your views here.
 
 def index(request):
-    all_data = LinkyData.objects.all()
-    currentSettings = Settings.objects.get(id=1)
-    tarifHPwh= currentSettings.tarifHP
-    conso = LinkyData.objects.latest('date').base - LinkyData.objects.get(id=1).base
+    #all_data = LinkyData.objects.all()
     consoTotal = LinkyData.objects.latest('date').base
-    prix = round((conso/1000)*tarifHPwh,2)
+    currentSettings = Settings.objects.get(id=1)
+    tarifHP = currentSettings.tarifHP
+    
+    now = LinkyData.objects.latest('date').date
+    delta24h = now - timedelta(hours=24)
+    last24h_data = LinkyData.objects.filter(date__gte=delta24h)
+    
+    if(currentSettings.freqPaiement == 'm'):
+        currentMonth_date = LinkyData.objects.filter(date__day=currentSettings.jourPaiement).latest('date').date
+        month_data = LinkyData.objects.filter(date__gte=currentMonth_date)
+        conso = LinkyData.objects.latest('date').base - month_data.first().base
+        if(currentSettings.abonnementHPHC):
+            tarifHC = currentSettings.tarifHC
+            
+            debutHC = currentSettings.debutHC
+            finHC = currentSettings.finHC
+            
+            consoHC = 0
+            consoHP = 0
+            prec_data = month_data.first()
+            for curr_data in month_data:
+                if(curr_data != prec_data):
+                    if(curr_data.date.time()>=debutHC or curr_data.date.time()<finHC):
+                        consoHC = consoHC + curr_data.base - prec_data.base
+                        prec_data = curr_data
+                    else:
+                        consoHP = consoHP + curr_data.base - prec_data.base
+                        prec_data = curr_data
+            
+            prix = round((consoHP/1000)*tarifHP,2)+round((consoHC/1000)*tarifHC,2)
+            
+        else:
+            prix = round((conso/1000)*tarifHP,2)
+    else:
+        """
+        DON'T USE !
+        
+        currentBimonthly_date = LinkyData.objects.filter(date__day=currentSettings.jourPaiement)[0].date
+        print(currentBimonthly_date)
+        bimonthly_data = LinkyData.objects.filter(date__gte=currentBimonthly_date)
+        
+        if(currentSettings.abonnementHPHC):
+            tarifHC = currentSettings.tarifHC
+        else:
+            conso = LinkyData.objects.latest('date').base - bimonthly_data.first().base
+            prix = round((conso/1000)*tarifHP,2)
+        """
+            
     return render(request, '_index_clean.html', locals())
 
 def settings(request):
