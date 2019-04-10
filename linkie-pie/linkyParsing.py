7e6769cc02a85877cba2dcbd26a265ff5f85f388
Florian Garcia <florian.garcia@outlook.fr>
2019-04-10 19:32:00 +0200

    add try catch

--- linkie-pie/linkyParsing.py
+++ linkie-pie/linkyParsing.py
@@ -2,17 +2,24 @@ import re
 import serial
 
 def cleanSerial(data):
-    data = data.decode()        #Convert into string
-    data = data.strip('\n\r')   #Clean
-    data = data.split(" ")      #Convert into array
-    line = []
-    if(len(data) >= 2):
-        line.append(data[0])
-        line.append(data[1])
-    else:
+    try:
+        data = data.decode()        #Convert into string
+        data = data.strip('\n\r')   #Clean
+        data = data.split(" ")      #Convert into array
+        line = []
+        if(len(data) >= 2):
+            line.append(data[0])
+            line.append(data[1])
+        else:
+            line.append("")
+            line.append("")
+        return line
+    except:
+        print("error with :"+data)
+        line = []
         line.append("")
         line.append("")
-    return line       
+        return line
 
 def linkyParsing(serialName):
     serialObj = serial.Serial(serialName, 1200, parity=serial.PARITY_EVEN, bytesize=7)
